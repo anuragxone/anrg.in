5dfdebd5485820a4a71d625e19a3df720fb1cdbf
Anurag Kumar <anuragxone@outlook.in>
2025-11-27 13:09:15 +0530

    chore: add tailwind + fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://anrg.in'
+  site: 'https://anrg.in',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
